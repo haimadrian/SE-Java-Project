7071046925d3a8df8e1504717b46e41478e22854
Lior Shor <liorshor123@gmail.com>
2020-06-07 10:20:06 +0300

    Cosmetic changes

--- Code/src/main/java/org/spa/ui/report/ReportView.java
+++ Code/src/main/java/org/spa/ui/report/ReportView.java
@@ -3,30 +3,22 @@ package org.spa.ui.report;
 import org.jdatepicker.impl.JDatePanelImpl;
 import org.jdatepicker.impl.JDatePickerImpl;
 import org.jdatepicker.impl.UtilDateModel;
-import org.spa.common.SPAApplication;
 import org.spa.common.util.log.Logger;
 import org.spa.common.util.log.factory.LoggerFactory;
-import org.spa.controller.item.ItemsWarehouse;
 import org.spa.controller.item.WarehouseItem;
 import org.spa.model.Order;
 import org.spa.model.report.EconomicReport;
 import org.spa.model.report.OrderReport;
 import org.spa.model.report.StockReport;
-import org.spa.ui.ManagerView;
 import org.spa.ui.control.PrintSupport;
 import org.spa.ui.util.DateLabelFormatter;
 import org.spa.ui.util.ImagesCache;
 
 import javax.swing.*;
-import javax.swing.border.CompoundBorder;
-import javax.swing.border.EmptyBorder;
-import javax.swing.border.TitledBorder;
 import java.awt.*;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import static java.time.LocalDate.now;
 
@@ -39,7 +31,8 @@ public class ReportView {
     JButton closeBtn;
     JButton printBtn;
     JLabel title;
-    JLabel datetxt;
+    JLabel selectStartDateLbl;
+    JLabel selectDayEndLbl;
     JPanel panel;
     JDatePickerImpl dateStart;
     JDatePickerImpl dateEnd;
@@ -50,7 +43,7 @@ public class ReportView {
         this.kindOfReport = kindOfReport;
         frame = new JFrame("Report View");
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        frame.setSize(900, 700);
+        frame.setSize(850, 600);
         frame.setLocationRelativeTo(null);
         panel = new JPanel();
         frame.add(panel);
@@ -65,21 +58,30 @@ public class ReportView {
         panel.setLayout(null);
         title = new JLabel();
         title.setFont(new Font("Arial", Font.PLAIN, 30));
-        reportText = new JTextArea();
-        datetxt = new JLabel();
-        reportText.setEditable(false);
+        title.setBounds(20, 1, 300, 70);
         panel.add(title);
-        panel.add(datetxt);
-        panel.add(reportText);
+        reportText = new JTextArea();
+        selectStartDateLbl = new JLabel("Select starting day:");
+        selectDayEndLbl = new JLabel("Select ending day:");
+        reportText.setEditable(false);
+        panel.add(selectStartDateLbl);
+        selectStartDateLbl.setBounds(20, 50, 200, 50);
+        selectStartDateLbl.setFont(new Font("Arial", Font.PLAIN, 20));
+        panel.add(selectDayEndLbl);
+        selectDayEndLbl.setBounds(20,275,200,50);
+        selectDayEndLbl.setFont(new Font("Arial", Font.PLAIN, 20));
         panel.add((closeBtn = new JButton("Close")));
-        closeBtn.setBounds(100, 600, 60, 50);
+        closeBtn.setBounds(80, 500, 60, 50);
         panel.add(printBtn = new JButton(ImagesCache.getInstance().getImage("Printer.png")));
-        printBtn.setBounds(180, 600, 60, 50);
-        title.setBounds(350, 2, 200, 70);
-        reportText.setBounds(330, 100, 500, 500);
+        printBtn.setBounds(160, 500, 60, 50);
+        panel.add(reportText);
+        reportText.setBounds(310, 5, 510, 550);
         UtilDateModel model = new UtilDateModel();
         model.setSelected(true);
         Properties p = new Properties();
+        p.put("text.today", "Today");
+        p.put("text.month", "Month");
+        p.put("text.year", "Year");
         UtilDateModel model2 = new UtilDateModel();
         model2.setSelected(true);
         Properties p2 = new Properties();
@@ -93,9 +95,9 @@ public class ReportView {
 
         dateEnd = new JDatePickerImpl(datePanel2, new DateLabelFormatter());
         panel.add(datePanel1);
-        datePanel1.setBounds(40, 150, 260, 200);
+        datePanel1.setBounds(20, 91, 260, 170);
         panel.add(datePanel2);
-        datePanel2.setBounds(40, 400, 260, 200);
+        datePanel2.setBounds(20, 315 , 260, 170);
         switch (kindOfReport) {
             case "Economic": {
                 title.setText("Economic Report");
@@ -117,29 +119,27 @@ public class ReportView {
         //Print
         printBtn.addActionListener(e -> PrintSupport.printComponent(reportText));
       /*  //Calander change
-        datePanel1.getModel().addChangeListener(changeEvent -> {
-        });
-        datePanel2.getModel().addChangeListener(changeEvent -> {
 
-        });*/
+        */
     }
 
     private void generateReport() {
         Random rand = new Random();
-        if(datePanel1.getModel().getDay() != 0 &&datePanel2.getModel().getDay() != 0 ) {
-            Date dateStart = new Date(datePanel1.getModel().getYear(), datePanel1.getModel().getMonth(), datePanel1.getModel().getDay());
-            Date dateEnd = new Date(datePanel2.getModel().getYear(), datePanel2.getModel().getMonth(), datePanel2.getModel().getDay());
+        if (kindOfReport.equals("Stock")){
+            int randomID = rand.nextInt(9999999);
+            StockReport stockReport = new StockReport(String.valueOf(randomID));
+            List<WarehouseItem> items = stockReport.getItems();
+            items.forEach(item -> {
+                reportText.setText(reportText.getText() + "Item name:\t" + item.getName() + "\n" + "Quantity:\t" + item.getCount() + "\t\t\t\n\n");
+            });
+            return;
+        }
+        datePanel1.getModel().addChangeListener(changeEvent -> {
+            reportText.setText("");
+            Date dateStart = new Date(datePanel1.getModel().getYear(), datePanel1.getModel().getMonth()+1, datePanel1.getModel().getDay());
+            Date dateEnd = new Date(datePanel2.getModel().getYear(), datePanel2.getModel().getMonth()+1, datePanel2.getModel().getDay());
             if (!dateStart.after(dateEnd)) {
                 switch (kindOfReport) {
-                    case "Stock": {
-                        int randomID = rand.nextInt(9999999);
-                        StockReport stockReport = new StockReport(String.valueOf(randomID));
-                        List<WarehouseItem> items = stockReport.getItems();
-                        items.forEach(item -> {
-                            reportText.setText(reportText.getText() + "Item name:\t" + item.getName() + "\n" + "Quantity:\t" + item.getCount() + "\t\t\t\n\n");
-                        });
-                        break;
-                    }
                     case "Order": {
                         int randomID = rand.nextInt(9999999);
                         OrderReport stockReport = new OrderReport(String.valueOf(randomID), dateStart, dateEnd);
@@ -157,14 +157,46 @@ public class ReportView {
                     }
                     case "Economic": {
                         int randomID = rand.nextInt(9999999);
-//                    EconomicReport economicReport = new EconomicReport(String.valueOf(randomID), dateStart, dateEnd);
+                        EconomicReport economicReport = new EconomicReport(String.valueOf(randomID), dateStart, dateEnd);
+
                         break;
                     }
                     default:
                         break;
                 }
             }
+        });
+        datePanel2.getModel().addChangeListener(changeEvent -> {
+        Date dateStart = new Date(datePanel1.getModel().getYear(), datePanel1.getModel().getMonth()+1, datePanel1.getModel().getDay());
+        Date dateEnd = new Date(datePanel2.getModel().getYear(), datePanel2.getModel().getMonth()+1, datePanel2.getModel().getDay());
+        reportText.setText("");
+        if (!dateStart.after(dateEnd)) {
+            switch (kindOfReport) {
+                case "Order": {
+                    int randomID = rand.nextInt(9999999);
+                    OrderReport stockReport = new OrderReport(String.valueOf(randomID), dateStart, dateEnd);
+                    Map<String, Order> orders = stockReport.getOrders();
+                    orders.values().stream().forEach(order -> {
+                        SimpleDateFormat sdf = new SimpleDateFormat("MMM dd,yyyy HH:mm");
+                        Date convertedDate = new Date(order.getOrderTime());
+                        reportText.setText(reportText.getText() + "Order ID: " + order.getOrderId() + "\tOrder date: " + sdf.format(convertedDate) + "\n");
+                        order.getItems().forEach(item -> {
+                            double discountPrice = item.getPrice() * item.getDiscountPercent() / 100;
+                            reportText.setText(reportText.getText() + "\tItem name: " + item.getName() + "\tQuantity:\t" + item.getCount() + (item.getPrice() - discountPrice) + "\n");
+                        });
+                    });
+                    break;
+                }
+                case "Economic": {
+                    int randomID = rand.nextInt(9999999);
+//                    EconomicReport economicReport = new EconomicReport(String.valueOf(randomID), dateStart, dateEnd);
+                    break;
+                }
+                default:
+                    break;
+            }
         }
+        });
     }
 }
 

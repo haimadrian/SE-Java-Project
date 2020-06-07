7a5bcefd9b78d3cca55480c96470080533f28ca3
Idan Pollak <idanpol1@gmail.com>
2020-06-08 00:40:35 +0300

    create ManagerView to be a responsive form

--- Code/src/main/java/org/spa/ui/ManagerView.java
+++ Code/src/main/java/org/spa/ui/ManagerView.java
@@ -13,7 +13,6 @@ import org.spa.ui.table.TableConfig;
 import org.spa.ui.table.TableManager;
 import org.spa.ui.util.Dialogs;
 import org.spa.ui.util.ImagesCache;
-
 import javax.swing.*;
 import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
@@ -25,205 +24,132 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-
 import static org.spa.ui.order.OrderCopying.orderToOrderViewInfo;
 import static org.spa.ui.order.OrderCopying.orderViewInfoToOrder;
 
 public class ManagerView {
     private static final Logger logger = LoggerFactory.getLogger(ManagerView.class);
     JFrame frame;
-  
-    public ManagerView() {
+    JPanel mainPanel;
+    private JLabel managerActions;
+    private JButton stockReport, orderReport, economicReport;
+    private TableManager<OrderColumn, OrderViewInfo> tableManager;
+    private List<OrderViewInfo> tableModelList;
+    private OrderSystem orderSystem;
+    private JTextField searchBar;
+    private JButton findOrderBtn;
 
+    public ManagerView() {
         frame = new JFrame("Management View");
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        frame.setSize(new Dimension(1300, 800));
-        SwingUtilities.invokeLater(() -> {
-            frame.setSize(new Dimension(1350, 850));
-        });
-        frame.add(new ManagementViewPane());
+        frame.setSize(new Dimension(1000, 650));
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+        mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+        frame.add(mainPanel);
+        panelsLayout();
     }
 
-    public class ManagementViewPane extends JPanel {
+    public void panelsLayout() {
+        JPanel titlePanel = new JPanel();
+        managerActions = new JLabel("Admin Actions");
+        managerActions.setFont(new Font("Arial", Font.PLAIN, 30));
+        titlePanel.add(managerActions);
+        mainPanel.add(titlePanel);
 
-        private TitlePane title;
-        private ReportsPane reportsPane;
-        private OrdersPane ordersPane;
+        // reportsPanel Layout
+        JPanel reportsPanel = new JPanel();
+        stockReport = new JButton("Stock Report");
+        reportsPanel.add(stockReport);
+        orderReport = new JButton("Order Report");
+        reportsPanel.add(orderReport);
+        economicReport = new JButton("Economic Report");
+        reportsPanel.add(economicReport);
+        reportsPanel.setBorder(new CompoundBorder(new TitledBorder("Reports"), new EmptyBorder(0, 0, 0, 0)));
+        mainPanel.add(reportsPanel);
 
 
-        public ManagementViewPane() {
-            setLayout(new GridBagLayout());
-            GridBagConstraints gbc = new GridBagConstraints();
-            gbc.gridx = 0;
-            gbc.gridy = 0;
-            gbc.weightx = 1;
-            gbc.weighty = 0.33;
-            gbc.anchor = GridBagConstraints.WEST;
-            gbc.fill = GridBagConstraints.BOTH;
-            gbc.insets = new Insets(4, 4, 4, 4);
+        // ordersPanel Layout
+        JPanel ordersPanel = new JPanel();
+        ordersPanel.setBorder(new CompoundBorder(new TitledBorder("Orders"), new EmptyBorder(0, 0, 0, 0)));
+        findOrderBtn = new JButton(ImagesCache.getInstance().getImage("Magnifying.png"));
+        ordersPanel.add(findOrderBtn);
 
-            add((title = new TitlePane()), gbc);
-            gbc.gridy++;
-            add((reportsPane = new ReportsPane()), gbc);
-            gbc.gridy++;
-            add((ordersPane = new OrdersPane()), gbc);
+        searchBar = new JTextField("Search by User/Order Id...");
+        ordersPanel.add(searchBar);
+        // add Orders table
+        createTable();
+
+        ordersPanel.add(tableManager.getMainPanel());
+        mainPanel.add(ordersPanel);
+
+        searchBar.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                searchBar.setText("");
+                refreshTable();
+            }
+        });
+
+        findOrderBtn.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                String searchString = "(?i).*" + searchBar.getText() + ".*";
+                List<Order> searchedItems = new ArrayList<>();
+                orderSystem.getOrdersMap().values().stream().forEach(order -> {
+                    if ((order.getOrderId().matches(searchString)) || (order.getUserId().matches(searchString))) {
+                        searchedItems.add(order);
+                    }
+                });
+                tableModelList.clear();
+                searchedItems.forEach(order -> tableModelList.add(orderToOrderViewInfo(order)));
+                tableManager.refresh();
+            }
+        });
+    }
+
+    private void refreshTable() {
+        tableModelList.clear();
+        orderSystem.getOrdersMap().values().stream().forEach(order -> tableModelList.add(orderToOrderViewInfo(order)));
+        try {
+            tableManager.refresh();
+        } catch (Throwable t) {
+            logger.error("Error has occurred while trying to refresh table.", t);
         }
     }
 
-    public class TitlePane extends JPanel {
-
-
-        public TitlePane() {
-            setLayout(new GridBagLayout());
-            GridBagConstraints gbc = new GridBagConstraints();
-            gbc.gridx = 0;
-            gbc.gridy = 0;
-
-            JLabel managerActions = new JLabel("Admin Actions");
-            managerActions.setFont(new Font("Arial", Font.PLAIN, 30));
-            add(managerActions, gbc);
-
-            gbc.fill = GridBagConstraints.HORIZONTAL;
-
-        }
-
-    }
-
-    public class ReportsPane extends JPanel {
-
-        private JButton stockReport, orderReport, economicReport;
-        private JLabel warehouse;
-
-        public ReportsPane() {
-            setLayout(new GridBagLayout());
-            setBorder(new CompoundBorder(new TitledBorder("Reports"), new EmptyBorder(0, 0, 0, 0)));
-            GridBagConstraints gbc = new GridBagConstraints();
-            gbc.anchor = GridBagConstraints.WEST;
-
-
-            JPanel panel = new JPanel(new GridBagLayout());
-            panel.add(new JLabel("Generate Report: "), gbc);
-            gbc.gridx++;
-            gbc.weightx = 1;
-            gbc.fill = GridBagConstraints.HORIZONTAL;
-            gbc.insets = new Insets(0, 0, 0, 0);
-            panel.add((warehouse = new JLabel()), gbc);
-
-            gbc.gridx = 0;
-            gbc.gridy = 0;
-            gbc.weightx = 1;
-            gbc.gridwidth = GridBagConstraints.REMAINDER;
-            gbc.fill = GridBagConstraints.HORIZONTAL;
-            gbc.insets = new Insets(4, 4, 4, 4);
-            add(panel, gbc);
-
-            gbc.gridwidth = 1;
-            gbc.weightx = 0.25;
-            gbc.gridy++;
-            gbc.fill = GridBagConstraints.HORIZONTAL;
-            add((stockReport = new JButton("Stock Report")), gbc);
-            gbc.gridx++;
-            add((orderReport = new JButton("Order Report")), gbc);
-            gbc.gridx++;
-            add((economicReport = new JButton("Economic Report")), gbc);
-        }
-    }
-
-    public class OrdersPane extends JPanel{
-        private TableManager<OrderColumn, OrderViewInfo> tableManager;
-        private List<OrderViewInfo> tableModelList;
-        private OrderSystem orderSystem;
-        JTextField searchBar;
-        JButton findOrderBtn;
-
-        public OrdersPane() {
-            setLayout(new GridBagLayout());
-            setBorder(new CompoundBorder(new TitledBorder("Orders"), new EmptyBorder(8, 0, 0, 0)));
-            GridBagConstraints gbc = new GridBagConstraints();
-            JPanel panel = new JPanel(new GridBagLayout());
-            add(panel, gbc);
-
-            // OrderPanel Layout
-            gbc.gridx = 0;
-            gbc.gridy = 0;
-            gbc.gridwidth = GridBagConstraints.REMAINDER;
-            gbc.fill = GridBagConstraints.HORIZONTAL;
-            gbc.insets = new Insets(4, 650, 15, 400);
-            gbc.fill = GridBagConstraints.HORIZONTAL;
-            findOrderBtn = new JButton(ImagesCache.getInstance().getImage("Magnifying.png"));
-            add(findOrderBtn,gbc);
-            gbc.insets = new Insets(4, 300, 15, 600);
-            add((searchBar = new JTextField("Search by User/Order Id...")), gbc);
-
-            // add Orders table
-            createTable();
-            add(tableManager.getMainPanel());
-
-            searchBar.addMouseListener(new MouseAdapter() {
-                @Override
-                public void mouseClicked(MouseEvent e) {
-                    searchBar.setText("");
-                    refreshTable();
-                }
-            });
-
-            findOrderBtn.addMouseListener(new MouseAdapter() {
-                @Override
-                public void mouseClicked(MouseEvent e) {
-                    String searchString = "(?i).*" + searchBar.getText() + ".*";
-                    List<Order> searchedItems = new ArrayList<>();
-                    orderSystem.getOrdersMap().values().stream().forEach(order -> {
-                        if ((order.getOrderId().matches(searchString)) || (order.getUserId().matches(searchString))) {
-                            searchedItems.add(order);
+    public void createTable() {
+        orderSystem = SPAApplication.getInstance().getOrderSystem();
+        List<OrderColumn> orderCols = Arrays.asList(OrderColumn.OrderId, OrderColumn.OrderTime, OrderColumn.Summary, OrderColumn.UserId);
+        TableConfig tableConfig = TableConfig.create().withLinesInRow(4).withEditable(true).withBorder(true).withColumnReordering(true).withColumnResizing(false).build();
+        tableModelList = new ArrayList<>();
+        tableManager = new TableManager<>(orderCols, tableModelList, tableConfig);
+        tableManager.getMainPanel().setPreferredSize(new Dimension(frame.getWidth() - 100, frame.getHeight() - 300));
+        tableManager.setFocusedRowChangedListener((rowNumber, selectedModel) -> {
+            logger.info("Selected model is: " + selectedModel);
+            orderSystem.getSelectionModel().setSelection(orderViewInfoToOrder(selectedModel));
+        });
+        tableManager.setPopupAdapter(new PopupAdapter() {
+            @Override
+            protected java.util.List<JMenuItem> getMenuItemsForPopup() {
+                JMenuItem item = new JMenuItem("View More...");
+                item.setDisplayedMnemonicIndex(0);
+                item.addActionListener(e -> {
+                    Order selection = orderSystem.getSelectionModel().getSelection();
+                    SwingUtilities.invokeLater(() -> {
+                        if (selection != null) {
+                            new OrderInfoDialog(orderToOrderViewInfo(selection)).init().setVisible(true);
+                        } else {
+                            Dialogs.showInfoDialog(frame, "No selection. Nothing to show.\nPlease select a row first.", "No selection");
                         }
                     });
-                    tableModelList.clear();
-                    searchedItems.forEach(order -> tableModelList.add(orderToOrderViewInfo(order)));
-                    tableManager.refresh();
-                }
-            });
-        }
-        private void refreshTable() {
-            tableModelList.clear();
-            orderSystem.getOrdersMap().values().stream().forEach(order -> tableModelList.add(orderToOrderViewInfo(order)));
-            try {
-                tableManager.refresh();
-            } catch (Throwable t) {
-                logger.error("Error has occurred while trying to refresh table.", t);
+                });
+                return Collections.singletonList(item);
             }
-        }
-        public void createTable() {
-            orderSystem = SPAApplication.getInstance().getOrderSystem();
-            List<OrderColumn> orderCols = Arrays.asList(OrderColumn.OrderId, OrderColumn.OrderTime, OrderColumn.Summary, OrderColumn.UserId);
-            TableConfig tableConfig = TableConfig.create().withLinesInRow(4).withEditable(true).withBorder(true).withColumnReordering(true).withColumnResizing(false).build();
-            tableModelList = new ArrayList<>();
-            tableManager = new TableManager<>(orderCols, tableModelList, tableConfig);
-            tableManager.getMainPanel().setPreferredSize(new Dimension(frame.getWidth() - 100, frame.getHeight() - 300));
-            tableManager.setFocusedRowChangedListener((rowNumber, selectedModel) -> {
-                logger.info("Selected model is: " + selectedModel);
-                orderSystem.getSelectionModel().setSelection(orderViewInfoToOrder(selectedModel));
-            });
-            tableManager.setPopupAdapter(new PopupAdapter() {
-                @Override
-                protected java.util.List<JMenuItem> getMenuItemsForPopup() {
-                    JMenuItem item = new JMenuItem("View More...");
-                    item.setDisplayedMnemonicIndex(0);
-                    item.addActionListener(e -> {
-                        Order selection = orderSystem.getSelectionModel().getSelection();
-                        SwingUtilities.invokeLater(() -> {
-                            if (selection != null) {
-                                new OrderInfoDialog(orderToOrderViewInfo(selection)).init().setVisible(true);
-                            } else {
-                                Dialogs.showInfoDialog(frame, "No selection. Nothing to show.\nPlease select a row first.", "No selection");
-                            }
-                        });
-                    });
-                    return Collections.singletonList(item);
-                }
-            });
-            refreshTable();
-        }
+        });
+        refreshTable();
     }
 }
+
+

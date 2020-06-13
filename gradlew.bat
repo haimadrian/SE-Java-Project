1702183f8ceb976295c7be70b2e7a4cb3f601686
Haim Adrian <Haim.Adrian@quest.com>
2020-06-13 23:24:56 +0300

    Revert "use specific path"
    This reverts commit 37dcc39743cf47ddbccd1cff9548f6fc75b3cea1.

--- gradlew.bat
+++ gradlew.bat
@@ -8,7 +8,7 @@
 @rem Set local scope for the variables with windows NT shell
 if "%OS%"=="Windows_NT" setlocal
 
-set DIRNAME=%~dp0
+set DIRNAME=./Code
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
@@ -51,7 +51,7 @@ if not "%OS%" == "Windows_NT" goto win9xME_args
 
 :win9xME_args
 @rem Slurp the command line arguments.
-set CMD_LINE_ARGS=-p %APP_HOME% clean build --info
+set CMD_LINE_ARGS=clean build --info
 set _SKIP=2
 
 :win9xME_args_slurp
@@ -62,9 +62,10 @@ set CMD_LINE_ARGS=%*
 :execute
 @rem Setup the command line
 
-set CLASSPATH=%APP_HOME%gradle\wrapper\gradle-wrapper.jar
+set CLASSPATH=gradle\wrapper\gradle-wrapper.jar
 
 @rem Execute Gradle
+cd %APP_HOME%\
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=SPAStore" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %CMD_LINE_ARGS%
 
 :end
